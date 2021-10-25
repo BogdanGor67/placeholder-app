8c72c92f82c844cf8f6a7e279f6623ad75ed83fe
BogdanGor67 <90646726+BogdanGor67@users.noreply.github.com>
2021-10-25 18:24:00 +0300

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/placeholder-app/' : '/',
-  configureWebpack: {
-    optimization: {
-      splitChunks: false
-    }
-  },
-};
